ba7e27cebb8e796be1b0b05372de9733fb4e61e5
p1v2 <pavliuk96@gmail.com>
2018-06-29 15:46:16 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='google_documents',
+      version='0.0.1',
+      description='Python package to work with Google Documents',
+      url='http://github.com/chimplie/google-documents',
+      author='Vitalii Pavliuk',
+      author_email='pavliuk96@gmail.com',
+      license='MIT',
+      packages=['google_documents'],
+      zip_safe=False)
