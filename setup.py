3f186f71de7cf2aa12bb1b8f9834a51f0e9f1618
Oleh Zorenko <lovemybeard@gmail.com>
2018-08-09 13:58:04 +0300

    0.0.1 release

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='google_documents',
-      version='0.0.1',
-      description='Python package to work with Google Documents',
-      url='http://github.com/chimplie/google-documents',
-      author='Vitalii Pavliuk',
-      author_email='pavliuk96@gmail.com',
-      license='MIT',
-      packages=['google_documents'],
-      zip_safe=False,
-      install_requires=[
-            "google-api-python-client==1.6.5",
-            "google-auth==1.4.1",
-            "google-auth-httplib2==0.0.3"
-      ]
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='google_documents',
+    version='0.0.1',
+    description='Python package to work with Google Documents',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/chimplie/google-documents',
+    author='Vitalii Pavliuk',
+    author_email='pavliuk96@gmail.com',
+    license='MIT',
+    packages=find_packages(exclude=['google_documents.tests', 'google_documents.tests.entities']),
+    zip_safe=False,
+    install_requires=[
+        "google-api-python-client==1.6.5",
+    ]
 )
 
