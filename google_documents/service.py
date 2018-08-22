78224b19bcaa26c656e6cb8a81ec39366736af6e
p1v2 <pavliuk96@gmail.com>
2018-08-22 19:01:16 +0300

    Add option to specify service account credentials in method call

--- google_documents/service.py
+++ google_documents/service.py
@@ -6,23 +6,29 @@ from googleapiclient import discovery
 
 
 # Google account scopes
-
 SCOPES = [
     'https://www.googleapis.com/auth/drive'  # Google Drive full access
 ]
 
-# Path to service.json file
-assert os.environ.get("GOOGLE_DOCUMENT_SERVICE_JSON"), "Google Documents Service account file not found. " \
-                                                   "You should specify it in $GOOGLE_DOCUMENT_SERVICE_JSON " \
-                                                   "environment variable."
-SERVICE_ACCOUNT_FILE = os.environ["GOOGLE_DOCUMENT_SERVICE_JSON"]
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+def get_service_account_credentials(google_service_account_file=None):
+    google_service_account_file = google_service_account_file or os.environ.get("GOOGLE_DOCUMENT_SERVICE_JSON")
+
+    assert google_service_account_file, "Google Documents Service account file not found. " \
+                                        "You should specify it via google_service_account_file or " \
+                                        "in $GOOGLE_DOCUMENT_SERVICE_JSON " \
+                                        "environment variable."
+
+    return ServiceAccountCredentials.from_json_keyfile_name(google_service_account_file, scopes=SCOPES)
 
 
-def get_drive_service():
+def get_drive_service(google_service_account_file=None):
+    credentials = get_service_account_credentials(google_service_account_file)
+
     return discovery.build('drive', 'v3', credentials=credentials, cache_discovery=False)
 
 
-def get_sheet_service():
+def get_sheet_service(google_service_account_file=None):
+    credentials = get_service_account_credentials(google_service_account_file)
+
     return discovery.build('sheets', 'v4', credentials=credentials, cache_discovery=False)
