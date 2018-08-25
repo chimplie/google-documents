4e1b7d3825b26ebdf8bccc57f7e73921fe978ba8
p1v2 <pavliuk96@gmail.com>
2018-08-25 19:53:18 +0300

    Fix tests

--- google_documents/entities/api_service_mixin.py
+++ google_documents/entities/api_service_mixin.py
@@ -2,7 +2,7 @@ from google_documents.entities.manager import GoogleDriveDocumentManager
 
 
 class ApiServiceMixin:
-    _custom_api_service: None
+    _custom_api_service = None
 
     def set_api_service(self, api_service):
         self._custom_api_service = api_service
