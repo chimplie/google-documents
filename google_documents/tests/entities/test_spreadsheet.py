9865ff196d14118071fa827d39a7543237d0ce89
p1v2 <pavliuk96@gmail.com>
2018-09-24 11:05:40 +0300

    Deleting test sheet even if tests have been failed

--- google_documents/tests/entities/test_spreadsheet.py
+++ google_documents/tests/entities/test_spreadsheet.py
@@ -41,30 +41,32 @@ class GoogleDriveSpreadsheetTestCase(TestCase):
 
         sheet = self.spreadsheet.sheets.create(title=SHEET_NAME)
 
-        # There should be sheet in the spreadsheet sheets
-        sheet_count_after_create = len(self.spreadsheet.sheets)
-        self.assertEqual(sheet_count_after_create - 1, sheet_count_initial)
+        try:
+            # There should be sheet in the spreadsheet sheets
+            sheet_count_after_create = len(self.spreadsheet.sheets)
+            self.assertEqual(sheet_count_after_create - 1, sheet_count_initial)
 
-        # Get item method should work
-        self.assertEqual(self.spreadsheet.sheets[SHEET_NAME], sheet)
+            # Get item method should work
+            self.assertEqual(self.spreadsheet.sheets[SHEET_NAME], sheet)
 
-        # Writing test data to the sheet
-        sheet[SHEET_RANGE_NAME] = TEST_DATA
-        # Reading
-        data = sheet[SHEET_RANGE_NAME]
+            # Writing test data to the sheet
+            sheet[SHEET_RANGE_NAME] = TEST_DATA
+            # Reading
+            data = sheet[SHEET_RANGE_NAME]
 
-        # Data should be equal to the data that has beed wrote
-        self.assertEqual(data, TEST_DATA)
+            # Data should be equal to the data that has beed wrote
+            self.assertEqual(data, TEST_DATA)
 
-        # Clearing data
-        sheet.clear(SHEET_RANGE_NAME)
-        # There should be no data
-        self.assertEqual(sheet[SHEET_RANGE_NAME], [])
+            # Clearing data
+            sheet.clear(SHEET_RANGE_NAME)
+            # There should be no data
+            self.assertEqual(sheet[SHEET_RANGE_NAME], [])
 
-        # Deleting sheet
-        sheet.delete()
+        finally:
+            # Deleting sheet
+            sheet.delete()
 
-        # Fetching sheets again
-        self.spreadsheet.sheets._sheets_objects = None
-        # There should be no sheet anymore
-        self.assertEqual(len(self.spreadsheet.sheets), sheet_count_initial)
+            # Fetching sheets again
+            self.spreadsheet.sheets._sheets_objects = None
+            # There should be no sheet anymore
+            self.assertEqual(len(self.spreadsheet.sheets), sheet_count_initial)
